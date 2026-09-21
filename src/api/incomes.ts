ff5531697c585fe55b9744687a5149e889c2f5af
Pakapol Tanachaisith <pakapol.tanachaisith@gmail.com>
2026-09-21 14:53:13 +0700

    Fix incomes fetching logic

--- src/api/incomes.ts
+++ src/api/incomes.ts
@@ -4,7 +4,11 @@ import { apiClient } from "./client";
 export const getIncomes = async (
   page: number,
 ): Promise<PaginatedResponse<Income>> => {
-  const response = await apiClient.get("/v1/incomes");
+  const response = await apiClient.get("/v1/incomes", {
+    params: {
+      page,
+    },
+  });
 
   return response.data;
 };
